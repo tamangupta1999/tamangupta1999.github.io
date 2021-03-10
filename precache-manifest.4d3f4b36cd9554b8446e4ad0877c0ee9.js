320d0d2f2c41b0699caa2001d733fdf8bdf3fe90
Taman Gupta <taman.gupta@tothenew.com>
2021-03-11 01:30:44 +0530

    final deployment of covid tracker

---
+++ precache-manifest.4d3f4b36cd9554b8446e4ad0877c0ee9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32412efd1f10e3d1b6bb2be603192ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e79b1ae58d60e74241b3",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "11af8cbc7cc484d846c2",
+    "url": "/static/css/main.96b1365c.chunk.css"
+  },
+  {
+    "revision": "e79b1ae58d60e74241b3",
+    "url": "/static/js/2.bb6d5597.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.bb6d5597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11af8cbc7cc484d846c2",
+    "url": "/static/js/main.2a177e32.chunk.js"
+  },
+  {
+    "revision": "5970e3a46d4b482a6929",
+    "url": "/static/js/runtime-main.2145eb05.js"
+  },
+  {
+    "revision": "0dc5cb9082e36d31deb4dde62aafe13a",
+    "url": "/static/media/Down.0dc5cb90.svg"
+  },
+  {
+    "revision": "6745a685a2bdefe0fa87c149d9845d68",
+    "url": "/static/media/Graph.6745a685.svg"
+  },
+  {
+    "revision": "834263463145e88a274ff1923010dcc5",
+    "url": "/static/media/Graph1.83426346.svg"
+  },
+  {
+    "revision": "13863221bfefe241dd548c634210e5a0",
+    "url": "/static/media/Graph2.13863221.svg"
+  },
+  {
+    "revision": "c84caed5ce3cdb68dde5bb7208e85903",
+    "url": "/static/media/Graph3.c84caed5.svg"
+  },
+  {
+    "revision": "5020d5ec68db3e5e114d94f01c08b029",
+    "url": "/static/media/Up.5020d5ec.svg"
+  },
+  {
+    "revision": "9e9072da649f1f397c08f6f0ff697103",
+    "url": "/static/media/UserDemo.9e9072da.svg"
+  },
+  {
+    "revision": "3fd044dc540c9eb96c997efa266137c7",
+    "url": "/static/media/faq.3fd044dc.png"
+  },
+  {
+    "revision": "1b8c1364b0c3b3092cfbd292ee8f3935",
+    "url": "/static/media/faq1.1b8c1364.png"
+  },
+  {
+    "revision": "dd2522806d986db0f7d74cc0efc741ee",
+    "url": "/static/media/twitter.dd252280.svg"
+  },
+  {
+    "revision": "d6ec837d9d70a98897868a73d7d92e0e",
+    "url": "/static/media/virus.d6ec837d.svg"
+  }
+]);
